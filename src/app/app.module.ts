81478dba422c72904fad149295f2c4b84ca6586f
ddarylmae <diieemmobee@gmail.com>
2018-05-19 14:06:52 +1200

    Added and integrated readings comp to app comp. Modified base markup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReadingsComponent } from './readings/readings.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ReadingsComponent
   ],
   imports: [
     BrowserModule
