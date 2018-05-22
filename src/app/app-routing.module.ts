638f74eefc1cfbeeefd1bcc1b3f65f59a924ea23
ddarylmae <diieemmobee@gmail.com>
2018-05-22 22:05:49 +1200

    Configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReadingsComponent } from './readings/readings.component';
 
 const routes: Routes = [
-  { path: 'categories', component: CategoriesComponent }
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'readings', component: ReadingsComponent }
 ];
 
 @NgModule({
