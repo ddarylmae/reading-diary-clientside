de019528b3e38f933ba555b4247c35e2e98910a4
Liana Pak <lianapaknz@gmail.com>
2018-06-08 11:54:34 +1200

    Implemented delete reading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { MatDialogModule,
 } from '@angular/material';
 import { StarComponent } from './shared/star.component';
 import { LoginComponent } from './login/login.component';
-
+import { DeleteConfirmationDialog } from './shared/delete-confirmation.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,7 +38,8 @@ import { LoginComponent } from './login/login.component';
     ReadingDetailsComponent,
     RegistrationComponent,
     StarComponent,
-    LoginComponent
+    LoginComponent,
+    DeleteConfirmationDialog
   ],
   imports: [
     BrowserModule,
@@ -57,6 +58,6 @@ import { LoginComponent } from './login/login.component';
   ],
   providers: [ CategoryService, MessageService, UserService, ReadingService, ScriptService ],
   bootstrap: [ AppComponent] ,
-  entryComponents: [ CategoryDetailsComponent, ReadingDetailsComponent ]
+  entryComponents: [ CategoryDetailsComponent, ReadingDetailsComponent, DeleteConfirmationDialog ]
 })
 export class AppModule { }
